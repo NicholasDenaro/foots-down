43475ffea1595bb89afc878c564c361966773e7b
Nicholas Denaro <ndenarodev@gmail.com>
2023-11-08 15:39:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function init(){
 	console.log('test');
-	let gyroscope = new AbsoluteOrientationSensor({
+	let gyroscope = new RelativeOrientationSensor({
 	//let gyroscope = new Gyroscope({
 		frequency: 60,
 		referenceFrame: 'device',
@@ -9,7 +9,7 @@ function init(){
 	gyroscope.onreading = () => {
 		//console.log(gyroscope);
 		//console.log('val');
-		//document.getElementById('gyro').innerText = Math.round(gyroscope.quaternion[1] * 100) / 100;
+		document.getElementById('gyro').innerText = Math.round(gyroscope.quaternion[1] * 100) / 100;
 		//document.getElementById('gyro').innerText = `x: ${gyroscope.x}, y: ${gyroscope.y}, z: ${gyroscope.z}`;
 		//document.getElementById('gyro').innerText = Math.round(tilt * 100) / 100;
 		let tiltval = gyroscope.quaternion[1];
