1e29b9aac3eb24914f1f46c43e93f9d6926da113
Nicholas Denaro <ndenarodev@gmail.com>
2023-11-08 15:24:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function init(){
 	gyroscope.onreading = () => {
 		//console.log(gyroscope);
 		//console.log('val');
-		document.getElementById('gyro').innerText = Math.round(gyroscope.quaternion[0] * 100) / 100;
+		document.getElementById('gyro').innerText = Math.round(gyroscope.quaternion[1] * 100) / 100;
 		//document.getElementById('gyro').innerText = `x: ${gyroscope.x}, y: ${gyroscope.y}, z: ${gyroscope.z}`;
 		/*document.getElementById('gyro').innerText = Math.round(tilt * 100) / 100;
 		if (playing && !debounce) {
